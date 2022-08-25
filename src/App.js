d0ce6542a2dd9989e4166d806db2d0d1bbc598ee
Alejandro Garcia <alejandrojg8@gmail.com>
2022-08-25 22:42:38 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/Routes/Home';
+import About from './components/Routes/About';
+import Skills from './components/Routes/Skills';
+import Portfolio from './components/Routes/Portfolio';
+import Contact from './components/Routes/Contact';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/PorfolioContainer.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+
+const Portfolio = () => {
+
+    const [navBar, setNavbar] = useState
+
+
+
+    return(
+        <>
+
+        </>
+    )
+}
+
+
+export default PortfolioContainer;
