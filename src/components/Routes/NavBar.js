819e507c4a3f485c1f22c7462c4f9fee3d40ce3b
Alejandro Garcia <alejandrojg8@gmail.com>
2022-08-27 18:37:12 +0100

    navbar done

--- src/components/Routes/NavBar.js
+++ src/components/Routes/NavBar.js
@@ -1,44 +1,91 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import './NavBar.css'
 
 
 
-const NavBar = () =>{
 
-  // const Ul = styled.ul`
-  // display: flex;
-  // justify-content: center;
-  // gap: 100px;
-  // font-size: 28px;
-  // padding: 30px;
-  // margin-right: 150px;
-  // `;
+const NavBar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu)
+  }
+
+  useEffect(() => {
+
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', changeWidth)
+
+    return () => {
+        window.removeEventListener('resize', changeWidth)
+    }
+
+  }, [])
 
   
-    return (
-      <nav>
+
+  const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 50px;
+  background: linear-gradient(45deg, rgb(156, 14, 156), midnightblue);
+  `
+
+  const NavUnlisted = styled.ul`
+    list-style-type: none;
+    background: linear-gradient(45deg, rgb(156, 14, 156), midnightblue);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    padding: 25px;
+    
+    a{
+      text-decoration: none;
+    }`
+
+  const Items = styled(Link)`
+   margin-right: 20px;
+    font-size: 20px;
+    text-transform: uppercase;
+    color: #f1f1f1;
+    cursor: pointer;`
+
+  const Toggle = styled.button`
+    display: none;
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    padding: 5px;
+    color: #000;
+    font-size: 18px;`
+  
+
+ 
 
 
-      
-        <ul className="list">
-      
-          <Link className="items" to="/">Home</Link>
-        
-      
-          <Link className="items" to="/about">About</Link>
-        
-      
-          <Link className="items" to="/portfolio">Portfolio</Link>
-        
-      
-          <Link className="items" to="/experience">Experience</Link>
-        
-          <Link className="items" to="/contact">Contact</Link>
-        
-      </ul>
-      </nav>
-    );
+  return (
+    <Nav>
+      {(toggleMenu || screenWidth > 500) && (
+        <NavUnlisted className="list">
+          <Items className="items" to="/">Home</Items>
+          <Items className="items" to="/about">About</Items>
+          <Items className="items" to="/portfolio">Portfolio</Items>
+          <Items className="items" to="/experience">Experience</Items>
+          <Items className="items" to="/contact">Contact</Items>
+        </NavUnlisted>
+      )}
+      <Toggle onClick={toggleNav} className="btn">BTN</Toggle>
+    </Nav>
+  )
 }
 
 export default NavBar;
